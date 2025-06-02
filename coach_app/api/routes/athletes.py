2f1e6a5ee39d21d63320a5ac91071911638bd9a1
Mello0229 <manipolpaulbyron@gmail.com>
2025-06-02 23:52:20 +0800

    gettattr

--- coach_app/api/routes/athletes.py
+++ coach_app/api/routes/athletes.py
@@ -13,7 +13,7 @@ async def get_athletes(coach=Depends(get_current_coach)):
         {
             "$match": {
                 "role": "athlete",
-                "profile.coach_name": coach.profile["name"]
+                "profile.coach_name": getattr(coach.profile, "name", None)
             }
         },
         {
